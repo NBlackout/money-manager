da58ce5c01d8a370f0fe7e7f267cbc719f453abc
Dimitri KOCH <dimitri.koch@agicap.com>
2023-05-06 22:52:21 +0200

    chore: disable transitive project references to make them explicit

--- Tests/MoneyManager.Client.Read.Application.Tests/GlobalUsings.cs
+++ Tests/MoneyManager.Client.Read.Application.Tests/GlobalUsings.cs
@@ -1,4 +1,2 @@
-﻿// Global using directives
-
-global using MoneyManager.Client.Read.Application.UseCases;
+﻿global using MoneyManager.Client.Read.Application.UseCases;
 global using MoneyManager.Shared.Presentation;
